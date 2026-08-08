51f2429000857ca09ef6d42266ee2329ab94398e
Kausik N <nkausik1999@gmail.com>
2026-08-08 22:13:30 +0530

    revamped using streamlit common utils

--- app.py
+++ app.py
@@ -4,191 +4,150 @@ Stream lit GUI for hosting AIVis
 
 # Imports
 import os
-import json
 import streamlit as st
 
 from AIVis import *
+from streamlit_common_utils.streamlit_common_ui_setup import *
 
 # Main Vars
-config = json.load(open("./StreamLitGUI/UIConfig.json", "r"))
+UI_CONFIG = UIConfig("./StreamLitGUI/UIConfig.json")
+UI_DATA = UIData("./StreamLitGUI/UIData.json")
+UI_CACHE = UICache(UI_DATA.get("paths.cache"))
 
 # Main Functions
 def main():
-    # Create Sidebar
-    selected_box = st.sidebar.selectbox(
-    "Choose one of the following",
-        tuple(
-            [config["PROJECT_NAME"]] + 
-            config["PROJECT_MODES"]
-        )
-    )
+    build_sidebar_app(UI_CONFIG, globals())
 
-    # Load Cache
-    LoadCache()
-    
-    if selected_box == config["PROJECT_NAME"]:
-        HomePage()
-    else:
-        correspondingFuncName = selected_box.replace(" ", "_").lower()
-        if correspondingFuncName in globals().keys():
-            globals()[correspondingFuncName]()
- 
-
-def HomePage():
-    st.title(config["PROJECT_NAME"])
-    st.markdown("Github Repo: " + "[" + config["PROJECT_LINK"] + "](" + config["PROJECT_LINK"] + ")")
-    st.markdown(config["PROJECT_DESC"])
-
-    # st.write(open(config["PROJECT_README"], "r").read())
 
 #############################################################################################################################
-# Repo Based Vars
-PATHS = {
-    "cache": "StreamLitGUI/CacheData/Cache.json",
-    "default": {
-        "datasets_dir": "StreamLitGUI/DefaultData/DefaultDatasets/",
-    },
-    "save": {
-        "csv": "StreamLitGUI/DefaultData/SavedDataset.csv"
-    }
-}
-
-# Util Vars
-CACHE = {}
-
 # Util Functions
-def LoadCache():
-    '''
-    Load Cache
-    '''
-    global CACHE
-    CACHE = json.load(open(PATHS["cache"], "r"))
-
-def SaveCache():
-    '''
-    Save Cache
-    '''
-    global CACHE
-    json.dump(CACHE, open(PATHS["cache"], "w"), indent=4)
-
 def LoadDefaultDatasets():
-    '''
+    """
     Load Default Datasets
-    '''
-    global CACHE
-    CACHE["default_datasets"] = []
-    for f in os.listdir(PATHS["default"]["datasets_dir"]):
-        if f.endswith(".csv"):
-            CACHE["default_datasets"].append(PATHS["default"]["datasets_dir"] + f)
-    SaveCache()
+    """
+    datasets_dir = UI_DATA.get("paths.default.datasets_dir", "Data/DefaultDatasets/")
+    dataset_paths = []
+    if os.path.isdir(datasets_dir):
+        for file_name in os.listdir(datasets_dir):
+            if file_name.endswith(".csv"):
+                dataset_paths.append(os.path.join(datasets_dir, file_name))
+    UI_CACHE["default_datasets"] = dataset_paths
+    UI_CACHE.save()
 
-    return CACHE["default_datasets"]
+    return dataset_paths
+
 
 def GetFileNames(file_paths):
-    '''
+    """
     Get File Names from File Paths
-    '''
+    """
     return [os.path.basename(file_path) for file_path in file_paths]
+
 
 # Main Functions
 def GenerateDatasetBasicInfo(USERINPUT_DatasetData):
-    '''
+    """
     Generate Basic Dataset Info
-    '''
-    Columns = USERINPUT_DatasetData.columns
-    ColumnsType = autodetect_column_types(USERINPUT_DatasetData)
-    ColumnsData = []
+    """
+    columns = USERINPUT_DatasetData.columns
+    columns_type = autodetect_column_types(USERINPUT_DatasetData)
+    columns_data = []
 
-    for i in range(len(Columns)):
-        c = Columns[i]
-        ColumnsData.append({
-            "name": Columns[i],
-            "type": ColumnsType[c],
+    for i in range(len(columns)):
+        column_name = columns[i]
+        columns_data.append({
+            "name": column_name,
+            "type": columns_type[column_name],
             "dtype": str(USERINPUT_DatasetData.dtypes[i]),
-            "categorizable": is_categorizable(USERINPUT_DatasetData, c, 25, 0.025),
-            "unique_values_count": len(USERINPUT_DatasetData[c].unique())
+            "categorizable": is_categorizable(USERINPUT_DatasetData, column_name, 25, 0.025),
+            "unique_values_count": len(USERINPUT_DatasetData[column_name].unique())
         })
 
-    DatasetBasicInfo = {
-        "columns_count": len(Columns),
+    dataset_basic_info = {
+        "columns_count": len(columns),
         "rows_count": USERINPUT_DatasetData.shape[0],
-        "columns_data": ColumnsData
+        "columns_data": columns_data
     }
 
-    return DatasetBasicInfo
+    return dataset_basic_info
+
 
 # UI Functions
 def UI_LoadDataset():
-    '''
+    """
     UI - Load Dataset
-    '''
-    DefaultDatasetPaths = LoadDefaultDatasets()
-    DefaultDatasetNames = GetFileNames(DefaultDatasetPaths)
-    DatasetNames = list(DefaultDatasetNames)
-    if os.path.exists(PATHS["save"]["csv"]): DatasetNames = ["Uploaded Dataset"] + DatasetNames
+    """
+    default_dataset_paths = LoadDefaultDatasets()
+    default_dataset_names = GetFileNames(default_dataset_paths)
+    dataset_names = list(default_dataset_names)
+    saved_csv_path = UI_DATA.get("paths.save.csv", "Data/SavedDataset.csv")
+    if os.path.exists(saved_csv_path):
+        dataset_names = ["Uploaded Dataset"] + dataset_names
 
-    USERINPUT_DatasetChoice = st.sidebar.selectbox("Choose a Dataset", DatasetNames)
+    USERINPUT_DatasetChoice = st.sidebar.selectbox("Choose a Dataset", dataset_names)
     if USERINPUT_DatasetChoice == "Uploaded Dataset":
-        USERINPUT_DatasetData = load_csv(PATHS["save"]["csv"])
+        USERINPUT_DatasetData = load_csv(saved_csv_path)
     else:
-        USERINPUT_DatasetData = load_csv(DefaultDatasetPaths[DefaultDatasetNames.index(USERINPUT_DatasetChoice)])
+        USERINPUT_DatasetData = load_csv(default_dataset_paths[default_dataset_names.index(USERINPUT_DatasetChoice)])
 
     return USERINPUT_DatasetData, USERINPUT_DatasetChoice
 
-def UI_DatasetDetails(DatasetBasicInfo):
-    '''
+
+def UI_DatasetDetails(dataset_basic_info):
+    """
     UI - Dataset Details
-    '''
+    """
     st.markdown("## Column Details")
-    colSize = (1, 3)
+    col_size = (1, 3)
 
-    col1, col2 = st.columns(colSize)
+    col1, col2 = st.columns(col_size)
     col1.markdown("Column Count")
-    col2.markdown(str(DatasetBasicInfo["columns_count"]))
+    col2.markdown(str(dataset_basic_info["columns_count"]))
 
-    col1, col2 = st.columns(colSize)
+    col1, col2 = st.columns(col_size)
     col1.markdown("Row Count")
-    col2.markdown(str(DatasetBasicInfo["rows_count"]))
+    col2.markdown(str(dataset_basic_info["rows_count"]))
 
-def UI_DisplayColumnDetails(DatasetBasicInfo):
-    '''
+
+def UI_DisplayColumnDetails(dataset_basic_info):
+    """
     UI - Display Column Details
-    '''
-    ColumnsData = DatasetBasicInfo["columns_data"]
-    ColumnNames = [c["name"] for c in ColumnsData]
-    USERINPUT_ColumnChoice = st.selectbox("Choose a Data Column", ColumnNames)
-    ColumnData = ColumnsData[ColumnNames.index(USERINPUT_ColumnChoice)]
+    """
+    columns_data = dataset_basic_info["columns_data"]
+    column_names = [column["name"] for column in columns_data]
+    USERINPUT_ColumnChoice = st.selectbox("Choose a Data Column", column_names)
+    column_data = columns_data[column_names.index(USERINPUT_ColumnChoice)]
 
     st.markdown("## Column Details")
-    colSize = (1, 3)
+    col_size = (1, 3)
 
-    col1, col2 = st.columns(colSize)
+    col1, col2 = st.columns(col_size)
     col1.markdown("Column Name")
-    col2.markdown(ColumnData["name"])
+    col2.markdown(column_data["name"])
 
-    col1, col2 = st.columns(colSize)
+    col1, col2 = st.columns(col_size)
     col1.markdown("Column DataType")
-    col2.markdown(ColumnData["dtype"])
+    col2.markdown(column_data["dtype"])
 
-    col1, col2 = st.columns(colSize)
+    col1, col2 = st.columns(col_size)
     col1.markdown("Column Type")
-    col2.markdown(ColumnData["type"])
+    col2.markdown(column_data["type"])
 
-    col1, col2 = st.columns(colSize)
+    col1, col2 = st.columns(col_size)
     col1.markdown("Column Categorizable")
-    col2.markdown(":heavy_check_mark:" if ColumnData["categorizable"] else ":x:")
+    col2.markdown(":heavy_check_mark:" if column_data["categorizable"] else ":x:")
 
-    col1, col2 = st.columns(colSize)
+    col1, col2 = st.columns(col_size)
     col1.markdown("Unique Values Count")
-    uniqueCount = ColumnData["unique_values_count"]
-    totalCount = DatasetBasicInfo["rows_count"]
-    percent = round(((uniqueCount*100) / totalCount), 2)
-    latexCode = "$\\frac{}{} = {}$".format(
-        "{" + str(uniqueCount) + "}",
-        "{" + str(totalCount) + "}",
+    unique_count = column_data["unique_values_count"]
+    total_count = dataset_basic_info["rows_count"]
+    percent = round(((unique_count * 100) / total_count), 2)
+    latex_code = "$\\frac{}{} = {}$".format(
+        "{" + str(unique_count) + "}",
+        "{" + str(total_count) + "}",
         str(percent) + "\%"
     )
-    col2.markdown(latexCode)
+    col2.markdown(latex_code)
 
 
 # Repo Based Functions
@@ -200,17 +159,21 @@ def upload_dataset():
     USERINPUT_DatasetData = st.file_uploader("Upload Dataset", ["csv"])
 
     # Process Inputs
+    saved_csv_path = UI_DATA.get("paths.save.csv", "Data/SavedDataset.csv")
     if USERINPUT_DatasetData is not None:
-        open(PATHS["save"]["csv"], "wb").write(USERINPUT_DatasetData.read())
-    elif not os.path.exists(PATHS["save"]["csv"]):
+        with open(saved_csv_path, "wb") as file:
+            file.write(USERINPUT_DatasetData.read())
+    elif not os.path.exists(saved_csv_path):
         st.markdown("Upload a dataset :sweat_smile:")
         return
 
-    USERINPUT_DatasetData = load_csv(PATHS["save"]["csv"])
+    USERINPUT_DatasetData = load_csv(saved_csv_path)
 
     # Display Outputs
     st.markdown("## Uploaded Dataset")
-    if USERINPUT_DatasetData is not None: st.table(USERINPUT_DatasetData.head())
+    if USERINPUT_DatasetData is not None:
+        st.table(USERINPUT_DatasetData.head())
+
 
 def view_dataset():
     # Title
@@ -222,9 +185,10 @@ def view_dataset():
     # Display Outputs
     st.markdown("## " + DatasetName)
     col1, col2 = st.columns(2)
-    USERINPUT_DatasetShowRangeStart = col1.number_input("Display Row Start", 0, USERINPUT_DatasetData.shape[0]-1, 0, 1)
-    USERINPUT_DatasetShowRangeCount = col2.number_input("Display Row Count", 1, USERINPUT_DatasetData.shape[0]-USERINPUT_DatasetShowRangeStart, 1, 1)
-    st.table(USERINPUT_DatasetData.iloc[USERINPUT_DatasetShowRangeStart:USERINPUT_DatasetShowRangeStart+USERINPUT_DatasetShowRangeCount, :])
+    USERINPUT_DatasetShowRangeStart = col1.number_input("Display Row Start", 0, USERINPUT_DatasetData.shape[0] - 1, 0, 1)
+    USERINPUT_DatasetShowRangeCount = col2.number_input("Display Row Count", 1, USERINPUT_DatasetData.shape[0] - USERINPUT_DatasetShowRangeStart, 1, 1)
+    st.table(USERINPUT_DatasetData.iloc[USERINPUT_DatasetShowRangeStart:USERINPUT_DatasetShowRangeStart + USERINPUT_DatasetShowRangeCount, :])
+
 
 def dataset_basic_info():
     # Title
@@ -234,33 +198,36 @@ def dataset_basic_info():
     USERINPUT_DatasetData, DatasetName = UI_LoadDataset()
 
     # Process Inputs
-    DatasetBasicInfo = {}
-    if DatasetName in CACHE["dataset_basic_info"]:
+    dataset_basic_info = {}
+    dataset_info_cache = UI_CACHE.get("dataset_basic_info", {})
+    if DatasetName in dataset_info_cache:
         if st.button("Regenerate Dataset Info"):
             GeneratedText = st.empty()
             GeneratedText.markdown("Generating Dataset Info...")
-            DatasetBasicInfo = GenerateDatasetBasicInfo(USERINPUT_DatasetData)
-            CACHE["dataset_basic_info"][DatasetName] = DatasetBasicInfo
-            SaveCache()
+            dataset_basic_info = GenerateDatasetBasicInfo(USERINPUT_DatasetData)
+            dataset_info_cache[DatasetName] = dataset_basic_info
+            UI_CACHE["dataset_basic_info"] = dataset_info_cache
+            UI_CACHE.save()
             GeneratedText.markdown("Regenerated Dataset Info!")
         else:
-            DatasetBasicInfo = CACHE["dataset_basic_info"][DatasetName]
+            dataset_basic_info = dataset_info_cache[DatasetName]
     else:
         GeneratedText = st.empty()
         GeneratedText.markdown("Generating Dataset Info...")
-        DatasetBasicInfo = GenerateDatasetBasicInfo(USERINPUT_DatasetData)
-        CACHE["dataset_basic_info"][DatasetName] = DatasetBasicInfo
-        SaveCache()
+        dataset_basic_info = GenerateDatasetBasicInfo(USERINPUT_DatasetData)
+        dataset_info_cache[DatasetName] = dataset_basic_info
+        UI_CACHE["dataset_basic_info"] = dataset_info_cache
+        UI_CACHE.save()
         GeneratedText.markdown("Regenerated Dataset Info!")
-        
+
     # Display Outputs
     st.markdown("## Dataset Overview")
-    UI_DatasetDetails(DatasetBasicInfo)
+    UI_DatasetDetails(dataset_basic_info)
 
     st.markdown("## Columns Overview")
-    UI_DisplayColumnDetails(DatasetBasicInfo)
+    UI_DisplayColumnDetails(dataset_basic_info)
 
-    
+
 #############################################################################################################################
 # Run Code
 if __name__ == "__main__":
